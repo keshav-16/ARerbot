daddbeda9a731396aead4cb5126f339ba66f87f3
keshav-16 <keshav162001@gmail.com>
2023-01-19 23:12:21 +0530

    some minor changes and footer added

--- static/apps.js
+++ static/apps.js
@@ -6,3 +6,16 @@ document.getElementById('cont_col2').addEventListener('click', function() {
     location.href = 'indexgltf.html'
 }, false);
 
+const cr = document.getElementById("cr");
+
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+      cr.style.flexDirection = "column";
+    } else {
+      cr.style.flexDirection = "row";
+    }
+  }
+  
+  var x = window.matchMedia("(max-width: 700px)")
+  myFunction(x) // Call listener function at run time
+  x.addListener(myFunction) // Attach listener function on state changes
